eb9b772180a350bf3930969150975fe974908e7b
Danben Erick <danbencb08@outlook.es>
2024-05-27 17:16:46 -0500

    Modificado reportes

--- src/pages/Reporte/ReportePage.jsx
+++ src/pages/Reporte/ReportePage.jsx
@@ -23,56 +23,45 @@ const ReportePage = () => {
 
   const procesasReporte = async (params) => {
     console.log(params)
-
-
-    // if(params.USUARIO === 'ELI' && params.CLAVE === 'un1v3$1d4dn0p4g0d4r4$1nf0rm4t1c4dm1$10n') {
-      
-    // }else {
-    //   message.info('Credenciales erroneas')
-    //   return
-    // }
-    // if(params.USUARIO === 'YOS' && params.CLAVE === 'g3p$0uv!4rm10n0f1d$41tn4!d04m0a4d1np1') {
-    //   message.info('Credenciales erroneas')
-    //   return
-    // }else {
-
-    // }
-
-    params = {
-      PROCESO: params.PROCESO || '',
-      COD_CARRERA: params.COD_CARRERA || '',
-      TIPO_REPORTE: params.TIPO_REPORTE || '',
-    }
-    let resp
-    switch(params.TIPO_REPORTE){
-      case 'PP':
-        resp = await obtenerReportePrimerPuestosService(params)
-        await utilGenerarExcel(resp.data, [30,10,50,10,10,30,40])
-      break
-      case 'RP':
-        resp = await obtenerReportePagosService(params)
-        await utilGenerarExcel(resp.data, [30,15,50,25])
-      break
-      case 'RPA':
-        resp = await obtenerReportedeEstudiantesPorAula(params)
-        await utilGenerarExcel(resp.data, [50,10,20])
-      break
-      case 'RI':
-          resp = await obtenerReportedeInscritosPorProceso(params)
-          await utilGenerarExcel(resp.data, [30,20,50,25,25,25,40])
-      break
-      case 'RPC':
-        resp = await obtenerReportePorCarrerasService(params)
-        await utilGenerarExcel(resp.data, [30, 50, 10])
-      break
-      case 'RPS':
-        resp = await obtenerReportePorSedeService(params)
-        await utilGenerarExcel(resp.data, [30, 50, 10])
-      break
-      default:
-        console.log('Seleciona un opcion valida')
+    if(params.USUARIO === 'ELI' && params.CLAVE === 'Randomiser1-Contours1-Genevan2-Trysts1-Llanwrtyd5-Prece4-Bluffed2-Ledgers6-Darting1-Browbeat1') {
+      console.log(params, 'ingreso')
+      params = {
+        PROCESO: params.PROCESO || '',
+        COD_CARRERA: params.COD_CARRERA || '',
+        TIPO_REPORTE: params.TIPO_REPORTE || '',
+      }
+      let resp
+      switch(params.TIPO_REPORTE){
+        case 'PP':
+          resp = await obtenerReportePrimerPuestosService(params)
+          await utilGenerarExcel(resp.data, [30,10,50,10,10,30,40])
         break
-
+        case 'RP':
+          resp = await obtenerReportePagosService(params)
+          await utilGenerarExcel(resp.data, [30,15,50,25])
+        break
+        case 'RPA':
+          resp = await obtenerReportedeEstudiantesPorAula(params)
+          await utilGenerarExcel(resp.data, [50,10,20])
+        break
+        case 'RI':
+            resp = await obtenerReportedeInscritosPorProceso(params)
+            await utilGenerarExcel(resp.data, [30,20,50,25,25,25,40])
+        break
+        case 'RPC':
+          resp = await obtenerReportePorCarrerasService(params)
+          await utilGenerarExcel(resp.data, [30, 50, 10])
+        break
+        case 'RPS':
+          resp = await obtenerReportePorSedeService(params)
+          await utilGenerarExcel(resp.data, [30, 50, 10])
+        break
+        default:
+          console.log('Seleciona un opcion valida')
+          break
+      }
+    }else {
+      message.error('Contraseña incorrecta')
     }
 
   }
@@ -103,7 +92,6 @@ const ReportePage = () => {
           <Form.Item label="Usuario" name="USUARIO" rules={[{ required: true }]}>
             <Select
               options={[
-                {value: 'ELI', label:"Elizabeth"},
                 {value: 'YOS', label:'Yosminda'}
               ]}
             />
