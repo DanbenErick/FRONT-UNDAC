126fabdf9c11029277294057292f5b1305d843e2
Danben Erick <danbencb08@outlook.es>
2024-07-16 10:45:22 -0500

    Pagos del cordinador

--- src/pages/PagosCordinador/PagosCordinador.jsx
+++ src/pages/PagosCordinador/PagosCordinador.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Form, Select, Input, Tooltip, Image, message, Modal, Table, Tag } from 'antd';
+import { Button, Form, Input, message, Modal, Table, Tag } from 'antd';
 
 import '../ReporteCordinador/ReporteCordinadorPage.css'
+import './PagosCordinador.css'
 
 import {  obtenerTodosLosProcesosActivosForm } from '../../api/apiInpputs';
 import { CheckOutlined, ScheduleOutlined, UnlockOutlined } from '@ant-design/icons';
@@ -44,26 +45,31 @@ const PagosCordinadorPage = () => {
       title: 'DNI',
       dataIndex: 'DNI',
       key: 'DNI',
+      responsive: ['xs', 'sm', 'md', 'lg'],
     },
     {
       title: 'Nombres',
       dataIndex: 'NOMBRE_COMPLETO',
       key: 'NOMBRE_COMPLETO',
+      responsive: ['xs', 'sm', 'md', 'lg'],
     },
     {
       title: 'Codigo',
       dataIndex: 'CODIGO',
       key: 'CODIGO',
+      responsive: ['xs', 'sm', 'md', 'lg'],
     },
     {
       title: 'Monto',
       dataIndex: 'MONTO',
       key: 'MONTO',
+      responsive: ['xs', 'sm', 'md', 'lg'],
     },
     {
       title: 'Estado',
       dataIndex: 'ESTADO',
       key: 'ESTADOS',
+      responsive: ['xs', 'sm', 'md', 'lg'],
       render: (data) => {
         return data === 1 ? <Tag color="green">CONFIRMADO</Tag> : <Tag color="red">FALTA CONFIRMAR</Tag>
       } 
@@ -72,6 +78,7 @@ const PagosCordinadorPage = () => {
       title: 'ACCION',
       dataIndex: 'ACCION',
       key: 'ACCION',
+      responsive: ['xs', 'sm', 'md', 'lg'],
       render: (data, data2) => {
           if(data2.ESTADO !== 1) {
             return (<Button onClick={() => validarPago(data2.ID)} icon={<CheckOutlined />}></Button>)
@@ -105,9 +112,9 @@ const PagosCordinadorPage = () => {
           
         </Form>
       </div>
-      <Modal centered width={1000} title="Direccion de Admision" open={statusModal} onOk={() => setStatusModal(false)} onCancel={() => setStatusModal(false)}>
+      <Modal centered width={1000} title="Direccion de Admision" open={statusModal} onOk={() => setStatusModal(false)} onCancel={() => setStatusModal(false)} footer={null}>
         <div className="containerModalPagosCordinador">
-          <Table columns={columns} dataSource={dataTable} pageSize={1000} />
+          <Table columns={columns} dataSource={dataTable} pageSize={1000} pagination={false} scroll={{ x: 'max-content' }} />
         </div>
       </Modal>
     </div>
